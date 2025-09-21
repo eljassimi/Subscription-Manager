d7a70d746403343b9ef9aa71fedf14dd08df2a06
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-09-21 21:27:25 +0100

    SM-2:Configure JDBC connection (PostgreSQL)

---
+++ src/util/DbConnection.java
@@ -0,0 +1,15 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/SubscriptionManager";
+    private static final String USER = "root";
+    private static final String PASS = "123456789";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
