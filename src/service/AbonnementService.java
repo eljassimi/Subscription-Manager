358e941a8e0ef4628d8c520e4be0665d464156d1
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-09-22 16:56:59 +0100

    SM-8 : Create AbonnementService with the Functions

---
+++ src/service/AbonnementService.java
@@ -0,0 +1,21 @@
+package service;
+
+import dao.AbonnementDAO;
+import dao.implement.AbonnementDAOImpl;
+import entity.Abonnement;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class AbonnementService {
+
+    private AbonnementDAO dao = new AbonnementDAOImpl();
+
+    public void create(Abonnement a) throws Exception { dao.create(a); }
+    public Optional<Abonnement> findById(String id) throws Exception { return dao.findById(id); }
+    public List<Abonnement> findAll() throws Exception { return dao.findAll(); }
+    public List<Abonnement> findActive() throws Exception {
+        return dao.findAll().stream().filter(ab -> ab.getStatut().name().equals("ACTIVE")).collect(Collectors.toList());
+    }
+}
