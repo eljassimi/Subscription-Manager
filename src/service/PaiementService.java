0f94a244821e0bc7106db80dc16e44dcc417437a
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-09-23 11:08:17 +0100

    SM-9 Fit Function Montant Total Impayes Par Abonnement

--- src/service/PaiementService.java
+++ src/service/PaiementService.java
@@ -1,22 +1,40 @@
 package service;
 
+import dao.AbonnementDAO;
 import dao.PaiementDAO;
+import dao.implement.AbonnementDAOImpl;
 import dao.implement.PaimentDAOImpl;
+import entity.Abonnement;
 import entity.Paiement;
 import entity.StatutPaiement;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class PaiementService {
 
     private PaiementDAO dao =  new PaimentDAOImpl();
+    private AbonnementDAO abonnementDAO = new AbonnementDAOImpl();
 
     public void Enregistrer(Paiement p) throws Exception { dao.create(p);}
 
     public List<Paiement> detecterImpayes() throws Exception {
-        return dao.findAll().stream().filter(p -> p.getStatut() == StatutPaiement.NON_PAYE ).collect(Collectors.toList());
+        return dao.findAll().stream().filter(p -> p.getStatut() == StatutPaiement.NON_PAYE || p.getStatut() == StatutPaiement.EN_RETARD).collect(Collectors.toList());
+    }
+
+    public double montantTotalImpayesParAbonnement(String abonnementId) throws Exception {
+
+        Abonnement abonnement = abonnementDAO.findById(abonnementId)
+                .orElseThrow(() -> new Exception("Abonnement non trouve"));
+
+        List<Paiement> paiementsImpayes = dao.findByAbonnement(abonnementId).stream()
+                .filter(p -> p.getStatut() == StatutPaiement.NON_PAYE
+                        || p.getStatut() == StatutPaiement.EN_RETARD)
+                .collect(Collectors.toList());
+
+        return paiementsImpayes.size() * abonnement.getMontantMensuel();
     }
 
 }
