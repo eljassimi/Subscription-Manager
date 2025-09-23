486490364b61f732e2262f96a080e3126f7cacd9
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-09-23 09:15:02 +0100

    SM-9 : Fit function that detect the Impayees subscriptions and return a list

--- src/service/PaiementService.java
+++ src/service/PaiementService.java
@@ -3,10 +3,20 @@ package service;
 import dao.PaiementDAO;
 import dao.implement.PaimentDAOImpl;
 import entity.Paiement;
+import entity.StatutPaiement;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PaiementService {
 
     private PaiementDAO dao =  new PaimentDAOImpl();
+
     public void Enregistrer(Paiement p) throws Exception { dao.create(p);}
+
+    public List<Paiement> detecterImpayes() throws Exception {
+        return dao.findAll().stream().filter(p -> p.getStatut() == StatutPaiement.NON_PAYE ).collect(Collectors.toList());
+    }
 
 }
